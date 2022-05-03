7b9aa14535ca6198edd7b133bb0fd0fd1836cf30
Giorgio <giorgio.capaccetta@di.unito.it>
2022-05-03 18:06:04 +0200

    test implemetation for sorting method

--- c_ex/Es1/src/sort_algo_test.c
+++ c_ex/Es1/src/sort_algo_test.c
@@ -1,103 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../C_resource/unity.h"
+#include <string.h>
+#include "sort_library.h"
+#include "unity.h"
 
-/**
- * Test suite for library contains algorithm for sorting collections
- */
+#define STRING_SIZE 64
+#define ARR_CAPACITY 7
 
-struct record{
-    char id_field;
-    char* string_field;
-    int integer_field;
-    double float_field;
-};
-
-static int precedes_record_id_field(void* r1_p, void* r2_p){
-    if(r1_p == NULL){
-        fprintf(stderr, "precedes_record_id_field: the first parameter is a null pointer");
-        exit(EXIT_FAILURE);
+int comp_int(void *a, void *b){
+    int *int_a = (int *) a;
+    int *int_b = (int *) b;
+    if(*int_a < *int_b){
+        return 2;
+    }else if(*int_a > *int_b){
+        return 1;
     }
-    if(r2_p == NULL){
-        fprintf(stderr, "precedes_record_id_field: the second parameter is a null pointer");
-        exit(EXIT_FAILURE);
-    }
-    struct record *rec1_p = (struct record*)r1_p;
-    struct record *rec2_p = (struct record*)r2_p;
-    if(rec1_p->id_field < rec2_p->id_field){
-        return(1);
-    }
-    return(0);
+    return 0;
 }
 
-static int precedes_record_string_field(void* r1_p, void* r2_p){
-    if(r1_p == NULL){
-        fprintf(stderr, "precedes_record_string_field: the first parameter is a null pointer");
-        exit(EXIT_FAILURE);
-    }
-    if(r2_p == NULL){
-        fprintf(stderr, "precedes_record_string_field: the second parameter is a null pointer");
-    }
-    struct record *rec1_p = (struct record*)r1_p;
-    struct record *rec2_p = (struct record*)r2_p;
+int comp_float(void *a, void *b){
+    float *float_a = (float *) a;
+    float *float_b = (float *) b;
 
-    if(strcmp(rec1_p->string_field, rec2_p->string_field) < 0){
-        return(1);
+    if(*float_a < *float_b){
+        return 2;
+    }else if(*float_a > *float_b){
+        return 1;
     }
-    return(0);
+    return 0;
 }
 
-static int precedes_record_integer_field(void* r1_p, void* r2_p){
-    if(r1_p == NULL){
-        fprintf(stderr, "precedes_record_integer_field: the first parameter is a null pointer");
-        exit(EXIT_FAILURE);
+int comp_string(void *a, void *b){
+    char *char_a = (char *) a;
+    char *char_b = (char *) b;
+    if(strcmp(*char_a, *char_b) > 0){
+        return 2;
+    }else if(strcmp(*char_a, *char_b) < 0){
+        return 1;
     }
-    if(r2_p == NULL){
-        fprintf(stderr, "precedes_record_integer_field: the second parameter is a null pointer");
-        exit(EXIT_FAILURE);
-    }
-    struct record *rec1_p = (struct record*)r1_p;
-    struct record *rec2_p = (struct record*)r2_p;
-    if(rec1_p->integer_field < rec2_p->integer_field){
-        return(1);
-    }
-    return(0);
+    return 0;
 }
 
-static int precedes_recors_float_field(void* r1_p, void* r2_p){
-    if(r1_p == NULL){
-        fprintf(stderr, "precedes_record_float_field: the first parameter is a null pointer");
-        exit(EXIT_FAILURE);    
-    }
-    if(r2_p == NULL){
-        fprintf(stderr, "precedes_record_float_field: the second parameter is a null pointer");
-        exit(EXIT_FAILURE);
-    }
-    struct record *rec1_p = (struct record*)r1_p;
-    struct record *rec2_p = (struct record*) r2_p;
-    if(rec1_p->float_field < rec2_p->float_field){
-        return(1);
-    }
-    return(0);
+//static int arr[ARR_CAPACITY];
+
+void setUp(void) {
+    /*for(char i = 0; i < ARR_CAPACITY; i++){
+        arr[i] = i;
+    }*/
 }
 
-static struct record r1, r2, r3;
-
-void setUp(void){
-    r1.id_field = 2;
-    r1.string_field = "ciao";
-    r1.integer_field = 9;
-    r1.float_field = 78.651616;
-
-    r2.id_field = 6;
-    r2.string_field = "pippo";
-    r2.integer_field = 4;
-    r2.float_field = 1.916151;
-
-    r1.id_field = 1;
-    r1.string_field = "talpa";
-    r1.integer_field = 5;
-    r1.float_field = 50.7915;
+void tearDown(void) {
 }
 
-static void test_sort_algo_
+/*static void free_memory(void** array){
+    for(int i = 0; i < ARR_CAPACITY; i++){
+        Record *rec_p = (Record *) array[i];
+        free(rec_p->string_field);
+    }
+    free(array);    
+}*/
+
+void test_sorted_int_array() { 
+  int arr[] = {1,2,3,4,5,6,7};
+  int* array[] = {&arr[0],&arr[1],&arr[2],&arr[3],&arr[4],&arr[5],&arr[6]};
+  int* correct_array[] = {&arr[0],&arr[1],&arr[2],&arr[3],&arr[4],&arr[5],&arr[6]};
+  
+  insert_sort((void**)array,7,comp_int);
+  TEST_ASSERT_EQUAL_PTR_ARRAY((void**)correct_array, (void**)array,7);
+}
+
+void test_unsorted_int_array() {
+  int arr[] = {1,2,3,4,5,6,7};
+  int* array[] = {&arr[0],&arr[4],&arr[1],&arr[6],&arr[2],&arr[5],&arr[3]};
+  int* correct_array[] = {&arr[0],&arr[1],&arr[2],&arr[3],&arr[4],&arr[5],&arr[6]};
+
+  insertion_sort((void**)array,7,comp_int);
+  TEST_ASSERT_EQUAL_PTR_ARRAY((void**)correct_array,(void**) array,7);
+}
+
+void test_sorted_float_array() { 
+  float arr[] = {1.5, 2.3, 2.8, 3.15, 5.89, 5.95, 6.22};
+  float* array[] = {&arr[0],&arr[1],&arr[2],&arr[3],&arr[4],&arr[5],&arr[6]};
+  float* correct_array[] ={&arr[0],&arr[1],&arr[2],&arr[3],&arr[4],&arr[5],&arr[6]};
+
+  insertion_sort((void**)array,7,comp_float);
+  TEST_ASSERT_EQUAL_PTR_ARRAY((void**)correct_array, (void**)array,7);
+}
+
+void test_unsorted_float_array() {
+  float arr[] = {1.5, 2.3, 2.8, 3.15, 5.89, 5.95, 6.22};
+  float* array[] = {&arr[0],&arr[4],&arr[1],&arr[6],&arr[2],&arr[5],&arr[3]};
+  float* correct_array[] = {&arr[0],&arr[1],&arr[2],&arr[3],&arr[4],&arr[5],&arr[6]};
+
+  insertion_sort((void**)array,7,comp_float);
+  TEST_ASSERT_EQUAL_PTR_ARRAY((void**)correct_array, (void**)array,7);
+}
+
+void test_sorted_string_array() { 
+  char *arr[] = {"acqua","ciao","elefante","formica","nave","orologio","zattera"};
+  char** array[] = {&arr[0],&arr[1],&arr[2],&arr[3],&arr[4],&arr[5],&arr[6]};
+  char** correct_array[] = {&arr[0],&arr[1],&arr[2],&arr[3],&arr[4],&arr[5],&arr[6]};
+
+  insertion_sort((void**)array,7,comp_string);
+  TEST_ASSERT_EQUAL_PTR_ARRAY((void**)correct_array, (void**)array, 7);
+}
+
+void test_unsorted_string_array() {
+  char *arr[] = {"acqua","ciao","elefante","formica","nave","orologio","zattera"};
+  char** array[] = {&arr[0],&arr[4],&arr[1],&arr[6],&arr[2],&arr[5],&arr[3]};
+  char** correct_array[] = {&arr[0],&arr[1],&arr[2],&arr[3],&arr[4],&arr[5],&arr[6]};
+  insertion_sort((void**)array,7,comp_string);
+  TEST_ASSERT_EQUAL_PTR_ARRAY((void**)correct_array, (void**)array, 7);
+}
+
+
+int main()
+{
+  UNITY_BEGIN();
+
+  RUN_TEST(test_sorted_int_array);
+  RUN_TEST(test_unsorted_int_array);
+  RUN_TEST(test_sorted_float_array);
+  RUN_TEST(test_unsorted_float_array);
+  RUN_TEST(test_sorted_string_array);
+  RUN_TEST(test_unsorted_string_array);
+
+  return UNITY_END();
+}
